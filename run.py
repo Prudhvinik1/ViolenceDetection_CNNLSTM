ff474a60794efb3d7b3ed0e0f94ea53e919e042c
prudhvinik <prudhvikrishna.n16@iiits.in>
2019-09-14 19:35:59 +0530

    Update run.py

--- run.py
+++ run.py
@@ -185,9 +185,9 @@ def hyper_tune_network(dataset_name, epochs, batch_size, batch_epoch_ratio, figu
 # static parameter for the netwotk
 datasets_videos = dict(
     hocky=dict(hocky="/content/drive/My Drive/ConvLSTM_violence/data/raw_videos/hocky"),
-    #violentflow=dict(violentflow="/content/drive/My Drive/ConvLSTM_violence/data/raw_videos/violentflow")
-    #movies=dict(movies="/content/drive/My Drive/ConvLSTM_violence/data/raw_videos/movies")
-    #crimes=dict(crimes="/content/drive/My Drive/UCF-Anomaly-Detection-Dataset")
+    violentflow=dict(violentflow="/content/drive/My Drive/ConvLSTM_violence/data/raw_videos/violentflow")
+    movies=dict(movies="/content/drive/My Drive/ConvLSTM_violence/data/raw_videos/movies")
+    crimes=dict(crimes="/content/drive/My Drive/UCF-Anomaly-Detection-Dataset")
 )
 
 crop_dark = dict(
